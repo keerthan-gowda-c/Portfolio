ece979ded0dbe14dadcea71206e6961f7a7fc65c
unknown <keerthan-gowda-c>
2026-01-31 13:28:26 +0530

    portfolio updation

--- frontend/src/pages/Home/Homepage.jsx
+++ frontend/src/pages/Home/Homepage.jsx
@@ -1,9 +1,23 @@
-function HomePage(){
-    return<>
-    <h1>
-        Hi, I'm <br />
-        Keerthan Gowda C
-    </h1>
+import './HomePage.css';
+
+function HomePage() {
+    return <>
+    <section className='hi'>
+<section className='home-page d-flex justify-content'>
+    <img src="images/ckg.jpeg" alt="" />
+            <div className='text text-end'>
+                <h1 className=''>Hi, I'm <br /> Keerthan Gowda C</h1>
+                <br />
+                <h3 className='blockquote-footer'>I'm a <br />Developer and Designer</h3>
+                
+                
+
+            </div>
+        </section>
+    </section>
+        
+
+
     </>
 }
 
