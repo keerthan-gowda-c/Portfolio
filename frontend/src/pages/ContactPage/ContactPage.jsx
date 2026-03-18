71a154fbd9aeb1bf07bd4620e025c2eda2d10acd
unknown <keerthan-gowda-c>
2026-03-18 11:10:15 +0530

    portfolio

--- frontend/src/pages/ContactPage/ContactPage.jsx
+++ frontend/src/pages/ContactPage/ContactPage.jsx
@@ -14,23 +14,25 @@ export default function ContactPage() {
                     </h1>
                 </div >
                 </section>
-                
+                <form action="">
                 <div className="right-area w-100">
                     <div className="mb-3 ">
                         <label for="exampleFormControlInput1" className="form-label">Your Name</label>
-                        <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="First Name - Second Name" />
+                        <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="First Name - Second Name" required/>
                     </div>
                     <div className="mb-3 ">
                         <label for="exampleFormControlInput1" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required/>
                     </div>
                     <div className="mb-3">
                         <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" required></textarea>
                     </div>
 
                     <button className='btn btn-success'>Submit</button>
                 </div>
+                </form>
+                
             </div>
 
 
