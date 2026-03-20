a0e40f0d8e676e231f7372ceb00bed4d81d50fef
unknown <keerthan-gowda-c>
2026-03-20 13:36:22 +0530

    project card updated

--- frontend/src/data/projects.jsx
+++ frontend/src/data/projects.jsx
@@ -3,9 +3,25 @@ export const projects_list = [
         id: 1,
         title: 'Keyless Shield',
         thumbnail: '/images/keyless-shield.png',
-        description: 'Operating a solenoid lock without using keys'
+        description: 'Operating a solenoid lock without using keys',
+        github_link : 'https://github.com/keerthan-gowda-c/Home-Automation-Security-System',
 
-    }
-    
+    },
+    {
+        id : 2,
+        title: 'Gowdru Biryani Website',
+        thumbnail : '/images/Gowdru_biryani.jpg',
+        description:"Frontend Project using React js",
+        github_link :'https://github.com/keerthan-gowda-c/React-e-commerce2',
+        vercel_link :'https://react-e-commerce2.vercel.app/',
+    },
+    {
+        id : 3,
+        title: 'My Portfolio',
+        thumbnail : '/images/download.jpeg',
+        description:"Frontend Project using React js",
+        github_link :'https://github.com/keerthan-gowda-c/Portfolio',
+        vercel_link :'https://portfolio-blue-tau-ziy5mfn59y.vercel.app/',
+    },
 ]
 
