de2fee3db08b1f52d3871095631dae866be9892a
keerthan-gowda-c <ckgowda9741@gmail.com>
2026-08-28 08:43:31 +0530

    Work in Progress - Experience

--- frontend/src/pages/WorkExperience/ExperienceCard.jsx
+++ frontend/src/pages/WorkExperience/ExperienceCard.jsx
@@ -4,38 +4,50 @@ export default function ExperienceCard({ experiences }) {
     return (
         <>
 
-        <div className="experience-card card">
-                        <div className="card-body">
+            <div className="experience-card card">
+                <div className="card-body">
 
 
 
-                            <div className="experience-header">
-                                <div className="experience-role">
-                                    <span className="office-logo"><i className="bi bi-building"></i></span>
-                                    <h5 className="role-name">
-                                        {experiences.role}
-                                        <span className="experience-company">{experiences.company_name}</span></h5>
-                                        <div className="dot"></div>
-                                </div>
-                                <div className="experience-duration">
-                                <span>{experiences.from_date}</span>
-                                <span className="duration-separator">- Present</span>
+                    <div className="experience-header">
+                        <div className="experience-role">
+                            <div className="left-header">
+                                <span className="office-logo"><i className="bi bi-building"></i></span>
+                                <h5 className="role-name">
+                                    {experiences.role}
+                                    <span className="experience-company">{experiences.company_name}</span></h5>
+                               {/* <div className="active-dot">
+                               </div> */}
+ <div className="dot"></div>
+
                                
                             </div>
-                            </div>
 
-                            <div className="experience-responsibility">
-                                <h6>Responsibilities</h6>
 
-                                <ul>
-                                    {Object.values(experiences.responsibilities[0]).map((value, index) => (
-                                        <li key={index}>{value}</li>
-                                    ))}
-                                </ul>
+                            <div className="right-header">
+                                <div className="experience-duration">
+                                    <span>{experiences.from_date}<i class="bi bi-dash"></i></span>
+                                    <span className="duration-separator">Present</span>
+
+                                </div>
                             </div>
 
                         </div>
+
                     </div>
+
+                    <div className="experience-responsibility">
+                        <h6 className="responsibility-header">Responsibilities</h6>
+
+                        <ul>
+                            {Object.values(experiences.responsibilities[0]).map((value, index) => (
+                                <li key={index}>{value}</li>
+                            ))}
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
 
 
             {/* <div >
